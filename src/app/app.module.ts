06bfd3c895a82da84d96579323f02fad857370d8
beautylotus <beautylotus718@gmail.com>
2022-09-07 23:44:15 +0430

    add play-bar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PlayBarComponent } from './components/play-bar/play-bar.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -12,7 +11,6 @@ import { CatergoryItemsComponent } from './pages/catergory-items/catergory-items
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SidebarComponent,
     PlayBarComponent,
     HomeComponent,
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn = true;
+  @ViewChild('aside') sidebar!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    document.getElementById('close-btn')?.addEventListener('click', () => {
+      this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'none');
+    });
+
+    document.getElementById('menu-btn')?.addEventListener('click', () => {
+      console.log('clicked');
+      this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'flex');
+    });
   }
 
+  onCloseMenu() {
+    this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'none');
+  }
 }
