80811b7a5d385c5267447df407b48e81e1c33269
beautylotus <beautylotus718@gmail.com>
2022-09-08 21:31:03 +0430

    finisjed homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { MusicModel } from 'src/app/models/music.model';
 import { PlayListModel } from 'src/app/models/playList.model';
 import { MusicService } from 'src/app/servieces/music.service';
 import SwiperCore from 'swiper';
@@ -10,11 +11,15 @@ import SwiperCore from 'swiper';
 })
 export class HomeComponent implements OnInit,OnDestroy  {
   suggestedSubscription: Subscription | undefined;
+  newerMusicSubscription : Subscription | undefined;
   suggestedPlayLists : PlayListModel[] = [];
+  newerMusicData : MusicModel[] = [];
+  searchKey: string = '';
   constructor(private musicService: MusicService) { }
 
   ngOnInit(): void {
     this.getPlayList();
+    this.getNewerMusics();
   }
 
   getPlayList() {
@@ -23,12 +28,37 @@ export class HomeComponent implements OnInit,OnDestroy  {
       // this.isWait = true;
     });
   }
+
+  getNewerMusics(){
+    this.newerMusicSubscription = this.musicService.getCategroyItems(4).subscribe((res:any)=>{
+      this.newerMusicData = res.data;
+    })
+  }
   onSwiper([swiper]:any) {
     console.log(swiper);
   }
   onSlideChange() {
     console.log('slide change');
   }
+
+  doSearch(searchKey: string): void {
+    let result: MusicModel[] = [];
+    if (searchKey.length > 2) {
+      result = this.newerMusicData.filter((item) => {
+        // @ts-ignore
+        return !(item.title.trim().indexOf(this.searchKey.trim()) <= -1);
+      });
+    }
+    if (result.length > 0) {
+      this.newerMusicData = result;
+    }
+    else {
+      this.getNewerMusics();
+    }
+  }
+
+
+
   ngOnDestroy(): void {
     if (this.suggestedSubscription) {
       this.suggestedSubscription.unsubscribe();
